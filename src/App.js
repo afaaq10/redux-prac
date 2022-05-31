9bf0a93017c326e6f17517a9477ade6ec61f37a7
Afaaq <afaaqmajeed123@gmail.com>
2022-05-31 12:12:46 +0530

    added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello{count}</h1>
-      {!bool && <h1>hi{bool}</h1>}
+      {!bool ? <h1>Hello boolean is true</h1> : <h1>Boolean is false</h1>}
       <button onClick={() => dispatch(increment(5))}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
 
