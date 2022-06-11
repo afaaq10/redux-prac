f1bc34d743d9f5bd4d9a18302400e69f1965e668
Afaaq <afaaqmajeed123@gmail.com>
2022-06-11 16:04:24 +0530

    added

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -2,16 +2,33 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import ProductReducer from '../redux/reducers/productReducer'
+
 const ProductComponents = () => {
+
     const products = useSelector((state) => state.productReducer.product)
     const renderList = products.map((x) => {
-        console.log("this is a product", x)
+        console.log("this is a product", products)
         const { id, title, image, description, price, category,rating } = x
-
         
+      
         return (
-        
+            <>
             <div className="four wide column" key={id}>
+              {{id} == 0 ? (
+           
+           <div style={{ display: "flex", margin: "auto", position: "relative", top: "105px", fontSize: "30px", padding: "10px", color: "blue", padding:"8px",   }}>
+               <div>
+               <i style={{position:"relative", top:"20px"}} class="fa fa-refresh fa-spin" />
+                 <p style={{justifyContent: "space-between",position:"relative", top:"80px"}}> ...Loading</p> 
+               {/* <i className="fa fa-cog fa-spin" /> */}
+             
+               </div>
+              
+           </div>
+           
+       ) : (
+        
+            
                  <Link to={`/products/${id}`}>
 
               
@@ -40,7 +57,10 @@ const ProductComponents = () => {
 
 
          </Link>  
-        </div>
+       
+       )}
+       </div>
+        </>
      
                
     )
